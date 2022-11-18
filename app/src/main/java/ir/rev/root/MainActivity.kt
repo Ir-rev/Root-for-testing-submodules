1564cdc2ade8ca439b063c24817a9860283496c9
au.pervov <au.pervov@tensor.ru>
2022-11-18 19:54:27 +0700

    simple project

---
+++ app/src/main/java/ir/rev/root/MainActivity.kt
@@ -0,0 +1,11 @@
+package ir.rev.root
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
