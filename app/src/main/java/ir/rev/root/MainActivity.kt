8469c05b149d4beb333ddd0a6b4c5f074e9b1a79
au.pervov <au.pervov@tensor.ru>
2022-12-18 16:06:23 +0700

    save

--- app/src/main/java/ir/rev/root/MainActivity.kt
+++ app/src/main/java/ir/rev/root/MainActivity.kt
@@ -1,11 +1,14 @@
 package ir.rev.root
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
