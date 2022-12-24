7db69aef876f106bdd97a6f2fe95745a13bd3da1
au.pervov <au.pervov@tensor.ru>
2022-12-24 18:04:59 +0700

    save

--- app/src/main/java/ir/rev/root/MainActivity.kt
+++ app/src/main/java/ir/rev/root/MainActivity.kt
@@ -1,12 +1,24 @@
 package ir.rev.root
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import ir.rev.foodMaker.FoodPlugin
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        lifecycleScope.launch(Dispatchers.IO) {
+            FoodPlugin.initFoodDataBase(applicationContext)
+            Log.d("checkResult", "onCreate: ${FoodPlugin.getFoodListRepository().checkDataBaseInit()}")
+            withContext(Dispatchers.Main){
+                setContentView(R.layout.activity_main)
+            }
+        }
     }
 }
